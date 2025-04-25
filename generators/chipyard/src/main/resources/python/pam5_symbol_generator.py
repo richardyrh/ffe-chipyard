cbd05b0d910cfe6d8710006d6e1b706e33f76d43
-T.K.- <t_k_233@outlook.com>
2025-04-25 13:48:26 -0700

    ADD: error checking

--- generators/chipyard/src/main/resources/python/pam5_symbol_generator.py
+++ generators/chipyard/src/main/resources/python/pam5_symbol_generator.py
@@ -5,14 +5,19 @@ import matplotlib.pyplot as plt
 
 class Pam5SymbolGenerator:
     @staticmethod
-    def random(n_symbols: int) -> np.ndarray:
+    def add_zeros(symbols: np.ndarray, prepend_zeros: int, append_zeros: int) -> np.ndarray:
+        return np.concatenate([np.zeros(prepend_zeros), symbols, np.zeros(append_zeros)])
+
+    @staticmethod
+    def random(n_symbols: int, prepend_zeros: int = 10, append_zeros: int = 10) -> np.ndarray:
         """
         Generate a random PAM5 symbol sequence
         """
-        return np.random.randint(-2, 3, n_symbols, dtype=np.int8).astype(np.float32)
+        symbols = np.random.randint(-2, 3, n_symbols, dtype=np.int8).astype(np.float32)
+        return Pam5SymbolGenerator.add_zeros(symbols, prepend_zeros, append_zeros)
 
     @staticmethod
-    def alternate_01(n_symbols: int) -> np.ndarray:
+    def alternate_01(n_symbols: int, prepend_zeros: int = 10, append_zeros: int = 10) -> np.ndarray:
         """
         Generate a PAM5 symbol sequence that alternates between {-1, 0, 1, 0}
         """
@@ -21,10 +26,10 @@ class Pam5SymbolGenerator:
         symbols[1::4] = 0
         symbols[2::4] = 1
         symbols[3::4] = 0
-        return symbols
+        return Pam5SymbolGenerator.add_zeros(symbols, prepend_zeros, append_zeros)
 
     @staticmethod
-    def alternate_02(n_symbols: int) -> np.ndarray:
+    def alternate_02(n_symbols: int, prepend_zeros: int = 10, append_zeros: int = 10) -> np.ndarray:
         """
         Generate a PAM5 symbol sequence that alternates between {-2, 0, 2, 0}
         """
@@ -33,10 +38,10 @@ class Pam5SymbolGenerator:
         symbols[1::4] = 0
         symbols[2::4] = 2
         symbols[3::4] = 0
-        return symbols
+        return Pam5SymbolGenerator.add_zeros(symbols, prepend_zeros, append_zeros)
 
     @staticmethod
-    def alternate_012(n_symbols: int) -> np.ndarray:
+    def alternate_012(n_symbols: int, prepend_zeros: int = 10, append_zeros: int = 10) -> np.ndarray:
         """
         Generate a PAM5 symbol sequence that alternates between {-2, -1, 0, 1, 2, 1, 0, -1}
         """
@@ -49,10 +54,10 @@ class Pam5SymbolGenerator:
         symbols[5::8] = 1
         symbols[6::8] = 0
         symbols[7::8] = -1
-        return symbols
+        return Pam5SymbolGenerator.add_zeros(symbols, prepend_zeros, append_zeros)
 
     @staticmethod
-    def sine_wave(n_symbols: int) -> np.ndarray:
+    def sine_wave(n_symbols: int, prepend_zeros: int = 10, append_zeros: int = 10) -> np.ndarray:
         symbols = np.zeros(n_symbols, dtype=np.float32)
         symbols[0::8] = np.sin(0 * np.pi / 4)
         symbols[1::8] = np.sin(1 * np.pi / 4)
@@ -62,14 +67,14 @@ class Pam5SymbolGenerator:
         symbols[5::8] = np.sin(5 * np.pi / 4)
         symbols[6::8] = np.sin(6 * np.pi / 4)
         symbols[7::8] = np.sin(7 * np.pi / 4)
-        return symbols
+        return Pam5SymbolGenerator.add_zeros(symbols, prepend_zeros, append_zeros)
     
     @staticmethod
-    def sawtooth_wave(n_symbols: int) -> np.ndarray:
+    def sawtooth_wave(n_symbols: int, prepend_zeros: int = 10, append_zeros: int = 10) -> np.ndarray:
         symbols = np.zeros(n_symbols, dtype=np.float32)
         symbols[0::5] = -2
         symbols[1::5] = -1
         symbols[2::5] = 0
         symbols[3::5] = 1
         symbols[4::5] = 2
-        return symbols
+        return Pam5SymbolGenerator.add_zeros(symbols, prepend_zeros, append_zeros)
--- generators/chipyard/src/main/resources/python/run_simulation.py
+++ generators/chipyard/src/main/resources/python/run_simulation.py
@@ -13,7 +13,7 @@ np.random.seed(420)
 simulation_frequency = 125e6 # Hz
 sampling_frequency = 125e6 # Hz
 
-n_symbols = 100
+n_symbols = 10
 duration = n_symbols * (1 / sampling_frequency)
 
 print(f"Duration: {duration} s")
@@ -40,7 +40,7 @@ cable_length = 50
 
 channel = EthernetChannel(GenericEthernetCable, cable_length)
 
-pam5_signal_noisy = channel.attenuate(pam5_signal, simulation_frequency)
+pam5_signal_attenuated = channel.attenuate(pam5_signal, simulation_frequency)
 
 
 # create time axis
@@ -50,7 +50,7 @@ time_axis = time_axis / 8 * 1e9
 
 # plot the signal
 plt.plot(time_axis, pam5_signal)
-plt.plot(time_axis, pam5_signal_noisy)
+plt.plot(time_axis, pam5_signal_attenuated)
 
 # create symbol boundaries
 symbol_boundaries = np.arange(0, len(pam5_signal), simulation_frequency // sampling_frequency)
@@ -75,10 +75,17 @@ plt.close()
 gen = FirFilterGenerator()
 taps = gen.calculate_taps(channel_profile=GenericEthernetCable, cable_length=cable_length)
 
-result = gen.simulate(pam5_signal_noisy)
+result = gen.simulate(pam5_signal_attenuated)
+
+# cancel out the delay of the filter
+symb_delay = 2
+result = result[symb_delay:]
+pam5_symbols = pam5_symbols[:-symb_delay]
+pam5_signal_attenuated = pam5_signal_attenuated[:-symb_delay]
+
 
 # show the attenuation before and after the filter
-input_power = np.max(np.abs(pam5_signal_noisy))
+input_power = np.max(np.abs(pam5_signal))
 output_power = np.max(np.abs(result))
 
 print(f"Input Power: {input_power:.2f}")
@@ -89,19 +96,60 @@ attenuation_lsb = attenuation * 0xFF
 
 print(f"Attenuation: {10 * np.log10(attenuation):.2f} dB, {attenuation_lsb:.2f} LSB")
 
+
+def calculate_error_rate(result, pam5_symbols, thresholds):
+    # calculate the best threshold for symbol detection
+
+    classified_symbols = np.zeros(result.shape[0])
+
+    for t in range(result.shape[0]):
+        # classify symbol based on threshold
+        if result[t] < thresholds[0]:
+            classified_symbol = -2
+        elif result[t] < thresholds[1]:
+            classified_symbol = -1
+        elif result[t] < thresholds[2]:
+            classified_symbol = 0
+        elif result[t] < thresholds[3]:
+            classified_symbol = 1
+        else:
+            classified_symbol = 2
+        
+        classified_symbols[t] = classified_symbol
+
+    print(f"Classified Symbols: {classified_symbols}")
+    print(f"Pam5 Symbols: {pam5_symbols.astype(np.int8)}")
+
+    # calculate the error rate
+    error_rate = np.sum(classified_symbols != pam5_symbols) / result.shape[0]
+    print(f"Threshold: {thresholds}, Error Rate: {error_rate:.2f}")
+
+
+thresholds = np.array([-0.6, -0.2, 0.2, 0.6]) * 0.15
+
+calculate_error_rate(result, pam5_symbols, thresholds)
+
+
+
 plt.clf()
 plt.close()
 
+
 # upsample to 1ghz then plot
+upsampled_original = np.repeat(pam5_signal, 8)
 upsampled_points = np.repeat(pam5_signal, 8)
-upsampled_noisy_points = np.repeat(pam5_signal_noisy, 8)
+upsampled_attenuated_points = np.repeat(pam5_signal_attenuated, 8)
 # upsample results with smoothing
 result_upsampled = np.repeat(result, 8)
-upsampled_result = np.convolve(result_upsampled, np.ones(8) / 8, mode="valid")
+# upsampled_result = np.convolve(result_upsampled, np.ones(8) / 8, mode="valid")
 
-plt.plot(np.hstack([np.zeros(20), upsampled_points]))
-plt.plot(np.hstack([np.zeros(20), upsampled_noisy_points]))
-plt.plot(upsampled_result)
-plt.legend(["Sampled", "Sampled Noisy", "Filtered"])
+plt.plot(upsampled_original, label="Original")
+plt.plot(upsampled_points, label="Sampled")
+plt.plot(upsampled_attenuated_points, label="Sampled Attenuated")
+plt.plot(result_upsampled, label="Filtered")
+for i in range(len(thresholds)):
+    plt.axhline(y=thresholds[i], color="k", linestyle="--", alpha=0.5)
+
+plt.legend()
 plt.savefig("test_pam5_signal_filtered.png")
 
